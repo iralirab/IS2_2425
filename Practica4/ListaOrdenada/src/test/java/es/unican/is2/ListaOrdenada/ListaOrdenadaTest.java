ebc16f8f070e4793b0fde854bc1a0c21caef6d9b
Mateo Barilari Asteinza <mba361@alumnos.unican.es>
2025-03-26 12:02:31 +0100

    Estructura inicial

---
+++ Practica4/ListaOrdenada/src/test/java/es/unican/is2/ListaOrdenada/ListaOrdenadaTest.java
@@ -0,0 +1,14 @@
+package es.unican.is2.ListaOrdenada;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ListaOrdenadaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
